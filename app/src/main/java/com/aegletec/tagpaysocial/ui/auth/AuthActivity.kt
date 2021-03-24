8aa44ae0ff295472876fe06cb42d815f079388bb
alumichael <alumichael@gmail.com>
2021-03-24 22:49:01 +0100

    Auth viewModel and repository

---
+++ app/src/main/java/com/aegletec/tagpaysocial/ui/auth/AuthActivity.kt
@@ -0,0 +1,20 @@
+package com.aegletec.tagpaysocial.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aegletec.tagpaysocial.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+}
